d543800a25c6b78faa5c916ba5bd33dc849ca7ef
Cyril <1075887346@qq.com>
2019-07-16 23:49:05 +0800

    implement "obtain employee list" and passed test.

---
+++ src/main/java/com/tw/apistackbase/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.tw.apistackbase.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController()
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    List<Employee> employees = new ArrayList<>();
+    {
+        employees.add(new Employee(1,"lisi", 20, "male", 8888));
+        employees.add(new Employee(2,"wangwu", 20, "male", 8888));
+    }
+
+    @GetMapping
+    public ResponseEntity getAll(){
+        return ResponseEntity.ok().body(employees);
+    }
+}
---
+++ src/test/java/com/tw/apistackbase/controller/EmployeeControllerTest.java
@@ -0,0 +1,50 @@
+package com.tw.apistackbase.controller;
+import com.alibaba.fastjson.JSON;
+import com.jayway.jsonpath.JsonPath;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONString;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnAllEmployees() throws Exception {
+
+        MvcResult mvcResult = this.mockMvc.perform(get("/employees"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        JSONArray jsonArray = new JSONArray(mvcResult.getResponse().getContentAsString());
+        assertEquals(1, jsonArray.getJSONObject(0).getInt("id"));
+        assertEquals("lisi", jsonArray.getJSONObject(0).getString("name"));
+        assertEquals(20, jsonArray.getJSONObject(0).getInt("age"));
+        assertEquals("male", jsonArray.getJSONObject(0).getString("gender"));
+        assertEquals(8888, jsonArray.getJSONObject(0).getDouble("salary"));
+        assertEquals(2, jsonArray.length());
+    }
+}
